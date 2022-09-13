7ff530f447409a59a4de98ed6d5c86f3937e8258
JMComstock <jasoncomstock11@gmail.com>
2022-09-13 14:22:40 -0500

    added some strings and a new message

--- cSharpPractice/Program.cs
+++ cSharpPractice/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+string firstName = "Jason";
+string lastName = "Comstock";
+
+Console.WriteLine("Hello, my first name is " + firstName + " and my last name is " + lastName);
